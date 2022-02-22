e0a8bd282f32f5552bb644246be0b2e73f86d039
Seppe Teugels <r0888765@student.thomasmore.be>
2022-02-22 16:53:51 +0100

    list element en header apart

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -14,13 +15,18 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
-    @GetMapping("/venuedetails")
-    public String venuedetails(Model model) {
-        //Venue venue = new Venue("Boesj","https://facebook.com",100,false,true,false,false,"Mechelen",1);
-        Optional<Venue> venueFromDb = venueRepository.findById(1);
+    @GetMapping({"/venuedetails/{id}", "/venuedetails"})
+    public String venuedetails(Model model, @PathVariable (required = false) Integer id) {
+        if(id == null) return "venuedetails";
+        Optional<Venue> venueFromDb = venueRepository.findById(id);
         if (venueFromDb.isPresent()) {
             model.addAttribute("venue", venueFromDb.get());
         }
         return "venuedetails";
+    }
+
+    @GetMapping({"/venuelist"})
+    public String venuelist(Model model){
+        return "venuelist";
     }
 }
