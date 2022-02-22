5259a9883e3959a5c715c23393c756b9fc373197
Seppe Teugels <r0888765@student.thomasmore.be>
2022-02-22 17:12:17 +0100

    venues via database

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -27,6 +27,8 @@ public class VenueController {
 
     @GetMapping({"/venuelist"})
     public String venuelist(Model model){
+        final Iterable<Venue> allVenues = venueRepository.findAll();
+        model.addAttribute("venues",allVenues);
         return "venuelist";
     }
 }
