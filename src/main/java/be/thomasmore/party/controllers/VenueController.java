313971c5c8b774988640122e804b4319e10ce522
seppe <r0888765@student.thomasmore.be>
2022-03-03 22:57:51 +0100

    venue list filter with min and max working.

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -2,45 +2,78 @@ package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
 import be.thomasmore.party.repositories.VenueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
 @Controller
 public class VenueController {
+    private Logger logger = LoggerFactory.getLogger(VenueController.class);
+
     @Autowired
     private VenueRepository venueRepository;
 
     @GetMapping({"/venuedetails/{id}", "/venuedetails"})
-    public String venuedetails(Model model, @PathVariable (required = false) Integer id) {
-        if(id == null) return "venuedetails";
+    public String venuedetails(Model model, @PathVariable(required = false) Integer id) {
+        if (id == null) return "venuedetails";
         long allvenues = venueRepository.count();
-        model.addAttribute("allvenues",allvenues);
+        model.addAttribute("allvenues", allvenues);
         Optional<Venue> venueFromDb = venueRepository.findById(id);
         venueFromDb.ifPresent(venue -> model.addAttribute("venue", venue));
         return "venuedetails";
     }
 
-    @GetMapping({"/venuelist" , "/venuelist/{filter}"})
-    public String venuelist(Model model, @PathVariable (required = false) String filter){
+    @GetMapping({"/venuelist", "/venuelist/{filter}"})
+    public String venuelist(Model model, @PathVariable(required = false) String filter) {
         final Iterable<Venue> allVenues = venueRepository.findAll();
-        model.addAttribute("venues",allVenues);
+        model.addAttribute("venues", allVenues);
         long allvenuesconut = venueRepository.count();
-        model.addAttribute("allvenuescount",allvenuesconut);
-        Boolean showFilter = false;
-        if(filter == null){
-            model.addAttribute("showFilter",showFilter);
+        model.addAttribute("allvenuescount", allvenuesconut);
+        boolean showFilter = false;
+        if (filter == null) {
+            model.addAttribute("showFilter", showFilter);
+            return "venuelist";
+        } else {
             return "venuelist";
         }
-        else if(filter.equals("filter")){
-            showFilter = true;
-            model.addAttribute("showFilter",showFilter);
+    }
+
+    @GetMapping({"/venuelist/filter"})
+    public String venueListWithFilter(Model model, @RequestParam(required = false) Integer minCapacity, @RequestParam(required = false) Integer maxCapacity) {
+        logger.info(String.format("venueListWithFilter -- min=%d", minCapacity));
+        logger.info(String.format("venueListWithFilter -- max=%d", maxCapacity));
+        Iterable<Venue> venuesbetween = venueRepository.findByCapacityBetween(minCapacity,maxCapacity);
+        Iterable<Venue> venuesbigger= venueRepository.findBybigger(minCapacity);
+        Iterable<Venue> venuessmaller = venueRepository.findBySmaller(maxCapacity);
+        final Iterable<Venue> allVenues = venueRepository.findAll();
+        model.addAttribute("venues", allVenues);
+        long allvenuesconut = venueRepository.count();
+        model.addAttribute("allvenuescount", allvenuesconut);
+        boolean showFilter = true;
+        if(minCapacity == null && maxCapacity == null){
+            model.addAttribute("venues", allVenues);
+            model.addAttribute("showFilter", showFilter);
             return "venuelist";
+        }else if (minCapacity != null && maxCapacity == null){
+            model.addAttribute("showFilter", showFilter);
+            model.addAttribute("venues",venuesbigger);
+            return "venuelist";
+        }else if(minCapacity == null){
+            model.addAttribute("showFilter", showFilter);
+            model.addAttribute("venues",venuessmaller);
+        }else {
+            model.addAttribute("showFilter", showFilter);
+            model.addAttribute("venues",venuesbetween);
         }
+        model.addAttribute("showFilter", showFilter);
         return "venuelist";
     }
 }
--- src/main/java/be/thomasmore/party/repositories/VenueRepository.java
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -10,12 +10,13 @@ public interface VenueRepository extends CrudRepository<Venue,Integer> {
     Iterable<Venue> findByIndoor(boolean indoor);
     @Query
             ("SELECT v from Venue v where v.capacity >=:min and v.capacity<=:max")
-    Iterable<Venue> findByCapacityBetween (@Param("min") int min , @Param("max") int max);
+    Iterable<Venue> findByCapacityBetween (@Param("min") Integer minCapacity , @Param("max") Integer maxCapacity);
     @Query
             ("SELECT v from Venue v where v.capacity<=:max")
-    Iterable<Venue> findBySmaller( @Param("max") int max);
+    Iterable<Venue> findBySmaller( @Param("max") Integer maxCapacity);
+
     @Query
             ("SELECT v from Venue v where v.capacity >=:min")
-    Iterable<Venue> findByBigger( @Param("min") int min);
+    Iterable<Venue> findBybigger( @Param("min") Integer minCapacity);
 
 }
